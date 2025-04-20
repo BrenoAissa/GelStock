08eecd59cd41125cebadc5834e2fde1efbe8b03b
brenoaissa <brenoaugusto05@hotmail.com.br>
2025-04-20 15:04:10 -0300

    feat(data): configure GelStockDbContext

---
+++ Data/GelStockDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GelStock.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GelStock.Api.Data
+{
+    public class GelStockDbContext : DbContext
+    {
+        public GelStockDbContext(DbContextOptions<GelStockDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
