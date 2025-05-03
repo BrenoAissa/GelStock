8133f659a89c775cc8ebfdbeea3f177317f94dbd
brenoaissa <brenoaugusto05@hotmail.com.br>
2025-05-03 15:40:54 -0300

    test(ProdutoServiceTest): add unit test for CriarItemAsync with valid product

---
+++ GelStock.Tests/ProdutoServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using GelStock.Api.Data;
+using GelStock.Api.Models;
+using GelStock.Api.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace GelStock.Tests
+{
+    public class ProdutoServiceTests
+    {
+        private readonly ProdutoService _service;
+        private readonly GelStockDbContext _context;
+        public ProdutoServiceTests() 
+        {
+            var options = new DbContextOptionsBuilder<GelStockDbContext>()
+                .UseInMemoryDatabase(databaseName: "GelStockTestDb")
+                .Options;
+
+            _context = new GelStockDbContext(options);
+            _service = new ProdutoService(_context);
+        }
+
+        [Fact]
+        public async Task CriarItemAsync_ProdutoValido_DeveAdicionarComSucesso()
+        {
+            // Arrange
+            var produto = new Produto { Nome = "Produto Teste", Tipo = "Teste", Fabricante = "Teste", Quantidade = "1"};
+
+            // Act
+            await _service.CriarItemAsync(produto);
+
+            // Assert
+            var produtos = await _context.Produtos.ToListAsync();
+            Assert.Single(produtos);
+            Assert.Equal("Produto Teste", produtos[0].Nome);
+        }
+    }
+}
--- GelStock.Tests/obj/Debug/net9.0/GelStock.Tests.AssemblyInfo.cs
+++ GelStock.Tests/obj/Debug/net9.0/GelStock.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GelStock.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63908284edb20bd3ed85cb78313dd90fb3aa0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GelStock.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GelStock.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
