2eeb036be7cf3755ec726ed7a8414297ac477494
brenoaissa <brenoaugusto05@hotmail.com.br>
2025-05-03 15:38:16 -0300

    fix(Produto): change Quantidade type from string to int

--- GelStock.Api/Models/Produto.cs
+++ GelStock.Api/Models/Produto.cs
@@ -6,7 +6,7 @@
         public string Nome { get; set; }
         public string Tipo { get; set; }
         public string Fabricante { get; set; }
-        public string Quantidade { get; set; }
+        public int Quantidade { get; set; }
 
         public int usuarioId { get; set; }
         public Usuario usuario { get; set; }
